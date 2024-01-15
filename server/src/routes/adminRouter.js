5f832d56613212b48e4c55daecd0db76b00a6e33
Antia Varela <anmavarpe@outlook.es>
2024-01-15 12:23:40 +0100

    Add: endpoint changePwd (sin testear, necesaria revisión)

--- server/src/routes/adminRouter.js
+++ server/src/routes/adminRouter.js
@@ -3,8 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const login = require('../controllers/admin/login');
+const changePwd = require('../controllers/admin/changePwd');
+
+const authAdmin = require('../middlewares/authAdmin');
 
 
 router.post('/login', login);
+router.put('/update-password/:idAdmin', authAdmin, changePwd);
 
 module.exports = router;
