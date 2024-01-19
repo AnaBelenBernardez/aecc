1384bcb08762065f623f6760e5c324a6d3ddbce2
Alejandro Pacheco Ferro <138246057+AlejandroPachec@users.noreply.github.com>
2024-01-19 13:50:07 +0100

    Add addNews and newsSchema

---
+++ server/src/schemas/newsSchema.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+
+const newsSchema = Joi.object({
+  title: Joi.string().min(2).max(100).required().messages({
+    'string.empty': 'Tienes que escribir el título de la noticia.',
+    'string.min': 'El título de la noticia debe tener mínimo 2 caracteres.',
+    'string.max': 'El título de la noticia no puede tener más de 100 caracteres.',
+    'any.required': 'El título de la noticia es obligatorio.'
+  }),
+  content: Joi.string().min(2).max(1500).required().messages({
+    'string.empty': 'El contenido de la noticia es obligatorio.',
+    'string.min': 'El contenido de la noticia debe tener mínimo 2 caracteres.',
+    'string.max': 'El contenido de la noticia no puede tener más de 1500 caracteres.',
+    'any.required': 'El contenido de la noticia es obligatorio.'
+  }),
+  link: Joi.string().uri().messages({
+    'string.min': 'El link de la noticia debe tener más de 2 caracteres.',
+    'string.max': 'El link de la noticia no puede tener más de 500 caracteres.',
+    'string.uri': 'El link de la noticia debe ser una URL (https://www.ejemplo.com). Sólo válidas https, no http.'
+  })
+});
+
+module.exports = newsSchema;
