1e706ffaa99a62af97093f2d98e4089bce8f592e
Alejandro Pacheco Ferro <138246057+AlejandroPachec@users.noreply.github.com>
2024-01-24 14:30:03 +0100

    Update locations and round not found icon

--- client/frontend/src/app/not-found.jsx
+++ client/frontend/src/app/not-found.jsx
@@ -16,7 +16,7 @@ const NotFound = () => {
 
           <Link
             href="/"
-            className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-lg focus:outline-none bg-primaryGreen active:bg-red-600 hover:bg-red-700"
+            className="px-5 inline py-3 text-sm font-medium leading-5 shadow-2xl text-white transition-all duration-400 border border-transparent rounded-full focus:outline-none bg-primaryGreen active:bg-red-600 hover:bg-red-700"
           >
             Volver al inicio
           </Link>
