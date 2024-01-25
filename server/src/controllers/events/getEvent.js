20b263286e4503efce0dc38efc4a204845ffc6c5
Antia Varela <anmavarpe@outlook.es>
2024-01-25 10:58:51 +0100

    QuickFix: getEvent

--- server/src/controllers/events/getEvent.js
+++ server/src/controllers/events/getEvent.js
@@ -6,9 +6,9 @@ async function getEvent (req,res,next) {
 
         const {idEvent} = req.params;
 
-        const connect = await getPool();
+        const pool = await getPool();
 
-        const [event] = await connect.query(
+        /* const [event] = await connect.query(
             `
                 SELECT e.id, e.date_start, e.date_end, e.title, e.galician_title, e.content, e.galician_content, e.warning, e.warning_content, e.galician_warning_content, e.location, e.event_type, e.link, GROUP_CONCAT(ep.id) AS photos_ids, GROUP_CONCAT(ep.photo) AS event_photos, ep.event_id AS event_id_photo
                 FROM events e
@@ -30,16 +30,33 @@ async function getEvent (req,res,next) {
             event[0].event_photos = new Array(event[0].event_photos);
         }else{
             event[0].event_photos = event[0].event_photos.split(",");
-        }
+        } */
 
+        const [event] = await pool.query(
+            `
+                SELECT id, date_start, date_end, title, galician_title, content, galician_content, warning, warning_content, galician_warning_content, location, event_type, link
+                FROM events
+                WHERE id=?
+            `,[idEvent]
+
+        );
 
         if(!event.length){
             return next(generateError('No se ha podido encontrar el evento', 404));
         }
 
+        const [photos] = await pool.query(
+            `
+                SELECT *
+                FROM events_photos
+                WHERE event_id = ?
+            `,[idEvent]
+        );
+
+
         res.status(200).send({
             status: "OK",
-            data: event
+            data: event, photos
         });
     } catch(e){
         console.log(e);
