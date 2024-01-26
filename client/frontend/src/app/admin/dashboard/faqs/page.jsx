4b9b8caa5a8a9e1823f67cc38173dec47e72d5f0
Antia Varela <anmavarpe@outlook.es>
2024-01-26 13:20:45 +0100

    Add: DeleteFaq y Update: EditFaq

--- client/frontend/src/app/admin/dashboard/faqs/page.jsx
+++ client/frontend/src/app/admin/dashboard/faqs/page.jsx
@@ -5,6 +5,10 @@ import Image from 'next/image';
 import Loading from '../../../../components/loading/Loading';
 import { useEffect, useState } from 'react';
 import EditFaq from '../../../../components/modals/faqs/EditFaq';
+import DeleteFaq from '../../../../components/modals/faqs/DeleteFaq';
+import { useLoginStore } from '../../../../store';
+import { useRouter } from 'next/navigation';
+import { deleteFaqService } from '../../../../service';
 
 
 const FaqAdminPage = () => {
@@ -12,6 +16,13 @@ const FaqAdminPage = () => {
 	const [clickedEdit, setClickedEdit] = useState(false);
 	const [faqsList, setFaqsList] = useState([]);
 	const [faqId, setFaqId] = useState();
+	const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+
+	const router = useRouter();
+  const token = useLoginStore((state) => state.token);
+  if (!token) {
+    router.push("/admin");
+  }
 
 	useEffect(() =>{
 		setFaqsList(faqs);
@@ -24,32 +35,49 @@ const FaqAdminPage = () => {
 		setFaqId(id);
 	}
 
+	function openModalDelete(faqId){
+    setFaqId(faqId);
+    setDeleteModalOpen(true);
+  }
+
+	const handleClickDelete = async () => {
+    await deleteFaqService(faqId, token);
+		
+		const findFaq = faqsList.find((faq) => faq.id === parseInt(faqId));
+		const indexEditedFaq = faqsList.indexOf(findFaq);
+		const newFaqsList = [...faqsList];
+		newFaqsList.splice(indexEditedFaq, 1);
+
+    setFaqsList(newFaqsList);
+    setDeleteModalOpen(false);
+  };
+
 	return (
 	<main className="mx-10 my-10 flex flex-col gap-5">
 		<h1 className="text-4xl sm:text-6xl text-primaryGreen font-semibold">
 		Gestión de preguntas frecuentes (FAQs)
 		</h1>
 		<ol className='flex flex-col gap-5'>
+		{deleteModalOpen && <DeleteFaq handleClickDelete={handleClickDelete} setDeleteModalOpen={setDeleteModalOpen} /> }
 		{
 			faqsList.length > 0 ? 
 			faqsList.map((faq) => {
 					return (
-							<li key={faq.id} className='marker:text-primaryGreen marker:text-4xl marker:font-bold md:marker:text-5xl'>
+							<li key={faq.id} className='w-[90vw] h-[40vh] bg-secondLightGray p-4 rounded-xl shadow-xl flex flex-col justify-center marker:text-primaryGreen marker:text-4xl marker:font-bold md:marker:text-5xl'>
 								{
 									clickedEdit && faqId === faq.id ?
 										<EditFaq currentFaq={faq} faqsList={faqsList} setFaqsList={setFaqsList} faqId={faqId} setClickedEdit={setClickedEdit}/>
 									:
-										<div className="inline-flex space-x-3">
+										<>
 											<div>
 												<span className="text-primaryGreen font-semibold text-2xl uppercase">{faq.question}</span>
 												<p className='pt-4'>{faq.answer}</p>
 											</div>
-											<div className="flex-col space-x-3">
-												<button onClick={()=>handleClickEdit(faq.id)}><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 36 36"><path fill="currentColor" d="M33.87 8.32L28 2.42a2.07 2.07 0 0 0-2.92 0L4.27 23.2l-1.9 8.2a2.06 2.06 0 0 0 2 2.5a2.14 2.14 0 0 0 .43 0l8.29-1.9l20.78-20.76a2.07 2.07 0 0 0 0-2.92M12.09 30.2l-7.77 1.63l1.77-7.62L21.66 8.7l6 6ZM29 13.25l-6-6l3.48-3.46l5.9 6Z" class="clr-i-outline clr-i-outline-path-1"/><path fill="none" d="M0 0h36v36H0z"/></svg></button>
-												<button><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 36 36"><path fill="currentColor" d="M27.14 34H8.86A2.93 2.93 0 0 1 6 31V11.23h2V31a.93.93 0 0 0 .86 1h18.28a.93.93 0 0 0 .86-1V11.23h2V31a2.93 2.93 0 0 1-2.86 3" class="clr-i-outline clr-i-outline-path-1"/><path fill="currentColor" d="M30.78 9H5a1 1 0 0 1 0-2h25.78a1 1 0 0 1 0 2" class="clr-i-outline clr-i-outline-path-2"/><path fill="currentColor" d="M21 13h2v15h-2z" class="clr-i-outline clr-i-outline-path-3"/><path fill="currentColor" d="M13 13h2v15h-2z" class="clr-i-outline clr-i-outline-path-4"/><path fill="currentColor" d="M23 5.86h-1.9V4h-6.2v1.86H13V4a2 2 0 0 1 1.9-2h6.2A2 2 0 0 1 23 4Z" class="clr-i-outline clr-i-outline-path-5"/><path fill="none" d="M0 0h36v36H0z"/></svg></button>
+											<div className='self-end flex gap-4'>
+												<button onClick={()=>handleClickEdit(faq.id)} className='flex gap-4 items-center justify-center border border-primaryGreen py-2 px-6 mt-4 rounded-3xl font-bold text-sm text-primaryGreen'><Image src="/icons/editIcon.svg" width={24} height={24}></Image></button>
+												<button onClick={() => openModalDelete(faq.id)}className='flex gap-4 items-center justify-center border border-secondRed py-2 px-6 mt-4 rounded-3xl font-bold text-sm text-secondRed'><Image src="/icons/deleteIcon.svg" width={24} height={24}></Image></button>
 											</div>
-											
-										</div>
+										</>
 								}
 							</li>
 					)
--- client/frontend/src/components/modals/faqs/EditFaq.jsx
+++ client/frontend/src/components/modals/faqs/EditFaq.jsx
@@ -9,94 +9,83 @@ function EditFaq({currentFaq, faqsList, setFaqsList, faqId, setClickedEdit}) {
   const [galician_answer, setGalicianAnswer] = useState(currentFaq.galician_answer);
   const [errorEdit, setErrorEdit] = useState("");
 
-function handleChange(e){
-    const {name} = e.target;
-    const value = e.target.value;
+  function handleChange(e){
+      const {name} = e.target;
+      const value = e.target.value;
 
-    switch(name){
-        case 'question':
-            setQuestion(value);
-            break;
-        case 'galician_question':
-            setGalicianQuestion(value);
-            break;
-        case 'answer':
-            setAnswer(value);    
-            break;
-        case 'galician_answer':
-            setGalicianAnswer(value);    
-            break;
-        default: 
-            break;
-    }
-}
+      switch(name){
+          case 'question':
+              setQuestion(value);
+              break;
+          case 'galician_question':
+              setGalicianQuestion(value);
+              break;
+          case 'answer':
+              setAnswer(value);    
+              break;
+          case 'galician_answer':
+              setGalicianAnswer(value);    
+              break;
+          default: 
+              break;
+      }
+  }
 
-async function handleSubmit(e){
-    e.preventDefault();
+  async function handleSubmit(e){
+      e.preventDefault();
 
-    let editedFaq;
+      let editedFaq;
 
-    const defaultValueFaq = faqsList.find((faq) => faq.id === parseInt(faqId));
+      try{
+          const tokenJson = window.localStorage.getItem('login');
+          const tokenObj = JSON.parse(tokenJson);
 
-    if(!question || question.length === 0){
-        setQuestion(defaultValueFaq.question);
-    }
-    if(!galician_question || galician_question === 0){
-        setGalicianQuestion(defaultValueFaq.galician_question);
-    }
-    if(!answer || answer.length === 0){
-        setAnswer(defaultValueFaq.answer);
-    }
-    if(!galician_answer || galician_answer.length === 0){
-        setGalicianAnswer(defaultValueFaq.galician_answer);
-    }
+          editedFaq = await editFaqService(question, galician_question, answer, galician_answer, faqId, tokenObj.state.token);
+      }catch(e){
+          setErrorEdit(e.message);
+      } finally{
+          setClickedEdit(false);
 
-    try{
-        const tokenJson = window.localStorage.getItem('login');
-        const tokenObj = JSON.parse(tokenJson);
+          const findFaq = faqsList.find((faq) => faq.id === parseInt(faqId));
+          const indexEditedFaq = faqsList.indexOf(findFaq);
+          const newFaqsList = [...faqsList];
+          newFaqsList.splice(indexEditedFaq, 1, editedFaq);
 
-        editedFaq = await editFaqService(question, galician_question, answer, galician_answer, faqId, tokenObj.state.token);
-    }catch(e){
-        setErrorEdit(e.message);
-    } finally{
-        setClickedEdit(false);
-
-        const findFaq = faqsList.find((faq) => faq.id === parseInt(faqId));
-        const indexEditedFaq = faqsList.indexOf(findFaq);
-        const newFaqsList = [...faqsList];
-        newFaqsList.splice(indexEditedFaq, 1, editedFaq);
-
-        setFaqsList(newFaqsList);
-    }
-}
+          setFaqsList(newFaqsList);
+      }
+  }
   return (
     <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50'>
-      <div className='w-[90vw] h-[90vh] bg-secondLightGray p-4 rounded-xl shadow-xl flex flex-col justify-center'>
+      <div className='w-[90vw] h-[60vh] bg-secondLightGray p-4 rounded-xl shadow-xl flex flex-col justify-center'>
         <form onSubmit={handleSubmit}>
           <fieldset>
-              <ul>
+              <ul className="flex flex-col gap-6">
                   <li>
-                      Castellano
-                      <label htmlFor='question'>
-                          <input onChange={handleChange} type="text" name="question" id="question" required minLength={5} maxLength={300} className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" defaultValue={currentFaq.question}></input>
-                      </label>
-                      <label htmlFor='answer'>
-                          <input onChange={handleChange} type="text" name="answer" id="answer" required minLength={5} maxLength={300} className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" defaultValue={currentFaq.answer}></input>
-                      </label>
+                    <h3 className="text-primaryGreen font-bold text-lg">Formulario en castellano</h3>
+                    <label htmlFor='question' className='text-sm font-bold'>
+                        Pregunta
+                        <input onChange={handleChange} type="text" name="question" id="question" required minLength={5} maxLength={300} className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" defaultValue={currentFaq.question}></input>
+                    </label>
+                    <label htmlFor='answer' className='text-sm font-bold'>
+                        Respuesta
+                        <input onChange={handleChange} type="text" name="answer" id="answer" required minLength={5} maxLength={300} className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" defaultValue={currentFaq.answer}></input>
+                    </label>
                   </li>
                   <li>
-                      Gallego
-                      <label htmlFor='galician_question'>
-                          <input onChange={handleChange} type="text" name="galician_question" id="question" required minLength={5} maxLength={300} className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" defaultValue={currentFaq.galician_question}></input>
-                      </label>
-                      <label htmlFor='galician_answer'>
-                          <input onChange={handleChange} type="text" name="galician_answer" id="galician_answer" required minLength={5} maxLength={300} className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" defaultValue={currentFaq.galician_answer}></input>
-                      </label>
+                    <h3 className="text-primaryGreen font-bold text-lg">Formulario en gallego</h3>
+                    <label htmlFor='galician_question' className='text-sm font-bold'>
+                        Pregunta
+                        <input onChange={handleChange} type="text" name="galician_question" id="question" required minLength={5} maxLength={300} className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" defaultValue={currentFaq.galician_question}></input>
+                    </label>
+                    <label htmlFor='galician_answer' className='text-sm font-bold'>
+                        Resposta
+                        <input onChange={handleChange} type="text" name="galician_answer" id="galician_answer" required minLength={5} maxLength={300} className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50" defaultValue={currentFaq.galician_answer}></input>
+                    </label>
                   </li>
               </ul>
+              <button type="submit" className='border border-primaryGreen py-2 px-6 mt-6 rounded-3xl font-bold text-sm text-primaryGreen self-end'>Guardar cambios</button>
           </fieldset>
           {errorEdit && <p>{errorEdit}</p>}
-          <button type="submit">Guardar cambios</button>
         </form>
       </div>
     </div>
