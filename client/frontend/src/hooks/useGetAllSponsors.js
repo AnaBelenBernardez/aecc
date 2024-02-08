32c8cc08aa7260315f731ed6773e867adbf42588
Nanna <nanaa.gonzalez@gmail.com>
2024-02-08 11:31:21 +0100

    Hook for useGetAllSponsors

--- client/frontend/src/hooks/useGetAllSponsors.js
+++ client/frontend/src/hooks/useGetAllSponsors.js
@@ -1,7 +1,30 @@
-const { useState } = require('react')
+const { useState, useEffect } = require('react');
+const { getAllSponsorsService } = require('../service');
 
 const useGetAllSponsors = () => {
   const [sponsors, setSponsors] = useState();
   const [loading, setLoading] = useState();
   const [error, setError] = useState();
+
+  const loadSponsors = async () => {
+    try {
+      setLoading(true);
+
+      const data = await getAllSponsorsService();
+
+      setSponsors(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadSponsors();
+  }, []);
+
+  return { sponsors, loading, error };
 }
+
+export default useGetAllSponsors;
