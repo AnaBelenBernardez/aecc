5bd13e7c7529a7cd7f9760f4e983df81621d56e9
Nanna <nanaa.gonzalez@gmail.com>
2024-02-15 17:54:15 +0100

    Remove clog and comments

--- server/src/middlewares/authAdmin.js
+++ server/src/middlewares/authAdmin.js
@@ -17,22 +17,6 @@ const authAdmin = async (req, res, next) => {
       return next(generateError('Token incorrecto', 401));
     }
 
-    /* const [admin] = await connect.query(
-      `
-          SELECT last_auth_update
-          FROM admins
-          WHERE id=?
-      `,
-      [tokenInfo.id]
-  );
-
-  const lastAuthUpdate = new Date(admin[0].last_auth_update);
-  const timeStampCreateToken = new Date(tokenInfo.iat * 1000);
-
-  if(timeStampCreateToken < lastAuthUpdate){
-    return next(generateError('La sesión ha expirado. Debe volver a identificarse.', 401));
-  } */
-
     req.admin = tokenInfo;
 
     next();
