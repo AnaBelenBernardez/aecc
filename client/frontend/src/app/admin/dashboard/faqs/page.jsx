04100e0c48150471021acc5deed16138fccbd06a
Antia Varela <anmavarpe@outlook.es>
2024-02-29 12:19:46 +0100

    Fix: toast on banners/page and faqs/page / Add: placeholders on AddBannerModal and EditBannerModal inputs

--- client/frontend/src/app/admin/dashboard/faqs/page.jsx
+++ client/frontend/src/app/admin/dashboard/faqs/page.jsx
@@ -143,53 +143,56 @@ const FaqAdminPage = () => {
   };
 
 	return (
-	<main className="mx-10 my-10 flex flex-col gap-3">
-		<BlockScroll isModalOpen={clickedAdd || deleteModalOpen || clickedEdit}/>
-		<h1 className="text-4xl sm:text-5xl text-primaryGreen font-bold self-center">
-			Gestión de preguntas frecuentes (FAQs)
-		</h1>
-		<div className='flex p-4 justify-center'>
-				<button onClick={handleClickAdd} className='border-2 border-primaryGreen bg-primaryGreen rounded-3xl text-sm font-bold px-10 py-2 mb-6 lg:self-end lg:mb-2 hover:text-primaryBlack hover:bg-secondLightGray hover:border-primaryGreen'>
-					AÑADIR FAQ
-				</button>
-		</div>
-		<ol className='flex flex-col gap-5'>
-		{deleteModalOpen && <DeleteFaq handleClickDelete={handleClickDelete} setDeleteModalOpen={setDeleteModalOpen} /> }
-		{clickedAdd && <AddFaq setClickedAdd={setClickedAdd} faqsList={faqsList} setFaqsList={setFaqsList} setAddSuccess={setAddSuccess} setAddReject={setAddReject} setEditReject={setEditReject} token={token}/> }
-		{
-			faqsList.length > 0 ? 
-			faqsList.map((faq) => {
-					return (
-							<li key={faq.id} className='flex flex-col justify-between p-8 items-center shadow-md md:flex-row'>
-								{
-									clickedEdit && faqId === faq.id ?
-										<EditFaq currentFaq={faq} faqsList={faqsList} setFaqsList={setFaqsList} faqId={faqId} setClickedEdit={setClickedEdit} setEditSuccess={setEditSuccess} setEditReject={setEditReject} token={token}/>
-									:
-										<article className="lg:w-[100%]">
-											<div>
-												<span className="text-primaryGreen font-semibold text-xl text-justify">{faq.question}</span>
-												<p className='pt-4 text-justify'>{faq.answer}</p>
-											</div>
-											<div className='self-end flex gap-4 justify-end lg:w-[100%]'>
-												<button onClick={()=>handleClickEdit(faq.id)} className='flex gap-4 items-center justify-center border border-primaryGreen py-2 px-6 mt-4 rounded-3xl font-bold text-sm text-primaryGreen'><Image src="/icons/editIcon.svg" width={24} height={24} alt="Editar"></Image>EDITAR</button>
-												<button onClick={() => openModalDelete(faq.id)}className='flex gap-4 items-center justify-center border border-secondRed py-2 px-6 mt-4 rounded-3xl font-bold text-sm text-secondRed'><Image src="/icons/deleteIcon.svg" width={24} height={24} alt="Eliminar"></Image>ELIMINAR</button>
-											</div>
-										</article>
-								}
-							</li>
-					)
-				})
-			: 
-				<>
-					<div className='flex flex-col md:flex-row md:justify-center md:items-center'>
-					<Image src={'/image/noFaqsYet.svg'} width={300} height={300} alt='Todavía no hay FAQs' className='mb-6 md:mb-0'/>
-					<p className='text-balance md:pl-10 lg:w-1/2'>Actualmente no hay preguntas frecuentes en la web. Añade nuevas FAQs para ayudar a los usuarios.</p>
-					</div>
-				</>
-		}
-		</ol>
-		<Toaster />
-	</main>
+		<>
+			<main className="mx-10 my-10 flex flex-col gap-3">
+				<BlockScroll isModalOpen={clickedAdd || deleteModalOpen || clickedEdit}/>
+				<h1 className="text-4xl sm:text-5xl text-primaryGreen font-bold self-center">
+					Gestión de preguntas frecuentes (FAQs)
+				</h1>
+				<div className='flex p-4 justify-center'>
+						<button onClick={handleClickAdd} className='border-2 border-primaryGreen bg-primaryGreen rounded-3xl text-sm font-bold px-10 py-2 mb-6 lg:self-end lg:mb-2 hover:text-primaryBlack hover:bg-secondLightGray hover:border-primaryGreen'>
+							AÑADIR FAQ
+						</button>
+				</div>
+				<ol className='flex flex-col gap-5'>
+				{deleteModalOpen && <DeleteFaq handleClickDelete={handleClickDelete} setDeleteModalOpen={setDeleteModalOpen} /> }
+				{clickedAdd && <AddFaq setClickedAdd={setClickedAdd} faqsList={faqsList} setFaqsList={setFaqsList} setAddSuccess={setAddSuccess} setAddReject={setAddReject} setEditReject={setEditReject} token={token}/> }
+				{
+					faqsList.length > 0 ? 
+					faqsList.map((faq) => {
+							return (
+									<li key={faq.id} className='flex flex-col justify-between p-8 items-center shadow-md md:flex-row'>
+										{
+											clickedEdit && faqId === faq.id ?
+												<EditFaq currentFaq={faq} faqsList={faqsList} setFaqsList={setFaqsList} faqId={faqId} setClickedEdit={setClickedEdit} setEditSuccess={setEditSuccess} setEditReject={setEditReject} token={token}/>
+											:
+												<article className="lg:w-[100%]">
+													<div>
+														<span className="text-primaryGreen font-semibold text-xl text-justify">{faq.question}</span>
+														<p className='pt-4 text-justify'>{faq.answer}</p>
+													</div>
+													<div className='self-end flex gap-4 justify-end lg:w-[100%]'>
+														<button onClick={()=>handleClickEdit(faq.id)} className='flex gap-4 items-center justify-center border border-primaryGreen py-2 px-6 mt-4 rounded-3xl font-bold text-sm text-primaryGreen'><Image src="/icons/editIcon.svg" width={24} height={24} alt="Editar"></Image>EDITAR</button>
+														<button onClick={() => openModalDelete(faq.id)}className='flex gap-4 items-center justify-center border border-secondRed py-2 px-6 mt-4 rounded-3xl font-bold text-sm text-secondRed'><Image src="/icons/deleteIcon.svg" width={24} height={24} alt="Eliminar"></Image>ELIMINAR</button>
+													</div>
+												</article>
+										}
+									</li>
+							)
+						})
+					: 
+						<>
+							<div className='flex flex-col md:flex-row md:justify-center md:items-center'>
+							<Image src={'/image/noFaqsYet.svg'} width={300} height={300} alt='Todavía no hay FAQs' className='mb-6 md:mb-0'/>
+							<p className='text-balance md:pl-10 lg:w-1/2'>Actualmente no hay preguntas frecuentes en la web. Añade nuevas FAQs para ayudar a los usuarios.</p>
+							</div>
+						</>
+				}
+				</ol>
+			</main>
+			<Toaster />
+		</>
+	
 	);
 };
 
